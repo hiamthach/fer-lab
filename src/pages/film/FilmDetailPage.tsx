bb523fce2842e7b8d394745fae85b27a433e92c7
Ngoc Thach <dreon1008@gmail.com>
2023-11-01 10:53:32 +0700

    detail & about page

--- src/pages/film/FilmDetailPage.tsx
+++ src/pages/film/FilmDetailPage.tsx
@@ -1,53 +1,82 @@
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 
-import { filmData } from '@/data/filmData';
+import filmApi from '@/config/api/filmApi';
+import { Film } from '@/config/types/film.type';
 import NotFound from '@/pages/exception/NotFound';
+import { CircularProgress } from '@mui/material';
 
 const FilmDetailPage = () => {
   const { id } = useParams<{ id: string }>();
 
-  const film = filmData.find((film) => film.id === id);
+  const {
+    data: film,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['films', id],
+    queryFn: async () => {
+      if (!id) {
+        return undefined;
+      }
+      const res = await filmApi.getFilm(id);
+      return res as unknown as Film;
+    },
+    retry: false,
+  });
 
-  if (!film) {
+  if (isLoading) {
+    return (
+      <div className="flex min-h-[80vh] items-center justify-center w-full">
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  if (isError) {
     return <NotFound />;
   }
 
   return (
     <div className="w-full py-4">
-      <div className="w-full h-[50vh] aspect-video rounded-xl overflow-hidden relative">
-        <img src={film.image} alt={film.title} className="w-full h-full object-cover object-center" />
+      {film && (
+        <>
+          <div className="w-full h-[50vh] aspect-video rounded-xl overflow-hidden relative">
+            <img src={film.image} alt={film.title} className="w-full h-full object-cover object-center" />
 
-        <div className="absolute top-0 l-0 r-0 w-full h-full bg-black z-10 bg-opacity-70"></div>
+            <div className="absolute top-0 l-0 r-0 w-full h-full bg-black z-10 bg-opacity-70"></div>
 
-        <div className="absolute top-0 l-0 r-0 w-full h-full z-20 flex items-end justify-left">
-          <h1 className="text-white text-8xl font-bold opacity-80 whitespace-nowrap">{film.title}</h1>
-        </div>
-      </div>
-      <div className="flex flex-col py-4 gap-2">
-        <div className="dark:text-white">
-          <h4 className="font-semibold text-lg mb-1">Description:</h4>
-          <p className="text-base font-light opacity-80">{film.description}</p>
-        </div>
-        <div className="dark:text-white">
-          <h4 className="font-semibold text-lg mb-1">Year:</h4>
-          <p className="text-base font-light opacity-80">{film.year}</p>
-        </div>
-        <div className="dark:text-white">
-          <h4 className="font-semibold text-lg mb-1">Nation:</h4>
-          <p className="text-base font-light opacity-80">{film.nation}</p>
-        </div>
-        <div className="dark:text-white">
-          <h4 className="font-semibold text-lg mb-1">Trailer:</h4>
-          <div className="w-full aspect-video my-4">
-            <iframe
-              className="w-full h-full"
-              src={film.youtubeUrl}
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            ></iframe>
+            <div className="absolute top-0 l-0 r-0 w-full h-full z-20 flex items-end justify-left">
+              <h1 className="text-white text-8xl font-bold opacity-80 whitespace-nowrap">{film.title}</h1>
+            </div>
           </div>
-        </div>
-      </div>
+          <div className="flex flex-col py-4 gap-2">
+            <div className="dark:text-white">
+              <h4 className="font-semibold text-lg mb-1">Description:</h4>
+              <p className="text-base font-light opacity-80">{film.description}</p>
+            </div>
+            <div className="dark:text-white">
+              <h4 className="font-semibold text-lg mb-1">Year:</h4>
+              <p className="text-base font-light opacity-80">{film.year}</p>
+            </div>
+            <div className="dark:text-white">
+              <h4 className="font-semibold text-lg mb-1">Nation:</h4>
+              <p className="text-base font-light opacity-80">{film.nation}</p>
+            </div>
+            <div className="dark:text-white">
+              <h4 className="font-semibold text-lg mb-1">Trailer:</h4>
+              <div className="w-full aspect-video my-4">
+                <iframe
+                  className="w-full h-full"
+                  src={film.youtubeUrl}
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/pages/main/AboutPage.tsx
+++ src/pages/main/AboutPage.tsx
@@ -1,11 +1,73 @@
+import React from 'react';
+
 import usePageTitle from '@/hooks/usePageTitle';
+import AbcIcon from '@mui/icons-material/Abc';
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Grid3x3Icon from '@mui/icons-material/Grid3x3';
+import PhoneIcon from '@mui/icons-material/Phone';
+
+const infoList: {
+  key: string;
+  value: string;
+  type: 'text' | 'link';
+  icon: React.FC;
+}[] = [
+  {
+    key: 'ID',
+    value: 'SE173476',
+    type: 'text',
+    icon: Grid3x3Icon,
+  },
+  {
+    key: 'Full Name',
+    value: 'Le Ngoc Thach',
+    type: 'text',
+    icon: AbcIcon,
+  },
+  {
+    key: 'Phone',
+    value: '0901469725',
+    type: 'text',
+    icon: PhoneIcon,
+  },
+  {
+    key: 'Email',
+    value: 'ThachLNSE173476@fpt.edu.vn',
+    type: 'text',
+    icon: EmailIcon,
+  },
+  {
+    key: 'Github',
+    value: 'https://github.com/hiamthach/fer-lab',
+    type: 'link',
+    icon: GitHubIcon,
+  },
+];
 
 const AboutPage = () => {
   usePageTitle('About');
 
   return (
-    <div className="flex justify-center items-center h-full mt-5">
-      <h1 className="font-bold text-4xl text-primary">Lê Ngọc Thạch - LAB4</h1>
+    <div className="mt-5">
+      <h1 className="font-bold text-4xl text-primary">FER - LAB7</h1>
+
+      <ul className="mt-5 flex flex-col gap-4">
+        {infoList.map((item, i) => (
+          <div className="flex gap-2 items-center" key={i}>
+            <item.icon />
+            <div className="w-32">{item.key}:</div>
+
+            {item.type === 'link' ? (
+              <a href={item.value} target="_blank" className="hover:text-primary hover:underline">
+                {item.value}
+              </a>
+            ) : (
+              <span>{item.value}</span>
+            )}
+          </div>
+        ))}
+      </ul>
     </div>
   );
 };
